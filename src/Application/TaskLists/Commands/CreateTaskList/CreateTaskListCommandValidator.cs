95202b3b863e6ee98858efa317267c63e89f2978
Renan Barbosa <renan.q.barbosa@avanade.com>
2022-09-07 14:29:53 -0300

    feat: add application test

--- src/Application/TaskLists/Commands/CreateTaskList/CreateTaskListCommandValidator.cs
+++ src/Application/TaskLists/Commands/CreateTaskList/CreateTaskListCommandValidator.cs
@@ -6,21 +6,10 @@ namespace Application.TaskLists.Commands.CreateTaskList;
 
 public class CreateTaskListCommandValidator : AbstractValidator<CreateTaskListCommand>
 {
-    private readonly ITaskDbContext _context;
-
-    public CreateTaskListCommandValidator(ITaskDbContext context)
+    public CreateTaskListCommandValidator()
     {
-        _context = context;
-
         RuleFor(v => v.Title)
             .NotEmpty().WithMessage("Title é obrigatório.")
-            .MaximumLength(256).WithMessage("Title pode ter até 256 caracteres.")
-            .MustAsync(BeUniqueTitle).WithMessage("Já existe uma task com o mesmo nome");
-    }
-
-    public async Task<bool> BeUniqueTitle(string title, CancellationToken cancellationToken)
-    {
-        return await _context.TaskLists
-            .AllAsync(l => l.Title != title, cancellationToken);
+            .MaximumLength(256).WithMessage("Title pode ter até 256 caracteres.");
     }
 }
---
+++ tests/Application.IntegrationTests/TaskLists/Commands/CreateTaskListTests.cs
@@ -0,0 +1,46 @@
+﻿using Application.TaskLists.Commands.CreateTaskList;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.IntegrationTests.TaskLists.Commands;
+
+public class CreateTaskListTests
+{
+    [Fact(DisplayName = "DADO um comando vazio, QUANDO validar, DEVE retornar erro de campo obrigatório")]
+    [Trait("Application", "Create - TaskList")]
+    public async Task InvalidCreateCommand_TryValidate_ShouldReturnErrors()
+    {
+        // Arrange
+        var command = new CreateTaskListCommand();
+
+        // Act
+        var result = new CreateTaskListCommandValidator().Validate(command);
+
+        // Assert
+        Assert.Equal(1, result.Errors.Count);
+        Assert.Equal("Title é obrigatório.", result.Errors.FirstOrDefault().ErrorMessage);
+    }
+
+    [Fact(DisplayName = "DADO um comando com título inválido, QUANDO validar o comando, DEVE retornar erro de 256 caracteres")]
+    [Trait("Application", "Create - TaskList")]
+    public async Task InvalidCreateCommand_TryValidateTitle_ShouldReturnErrors()
+    {
+        // Arrange
+        var command = new CreateTaskListCommand
+        {
+            //286 caracteres
+            Title = "Mussum Ipsum, cacilds vidis litro abertis. Mé faiz elementum girarzis, " +
+                    "nisi eros vermeio.Praesent malesuada urna nisi, quis volutpat erat hendrerit non. " +
+                    "Nam vulputate dapibus.Si u mundo tá muito paradis? " +
+                    "Toma um mé que o mundo vai girarzis!In elementis mé pra quem é amistosis quis leo."
+        };
+
+        // Act
+        var result = new CreateTaskListCommandValidator().Validate(command);
+
+        // Assert
+        Assert.Equal(1, result.Errors.Count);
+        Assert.Equal("Title pode ter até 256 caracteres.", result.Errors.FirstOrDefault().ErrorMessage);
+    }
+}
