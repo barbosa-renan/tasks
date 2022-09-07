2cea77d34b5375faa17f2ae9245bc8950b191d89
Renan Barbosa <renan.q.barbosa@avanade.com>
2022-09-07 14:32:50 -0300

    refactor: tests

--- tests/Application.IntegrationTests/TaskLists/Commands/CreateTaskListTests.cs
+++ tests/Application.IntegrationTests/TaskLists/Commands/CreateTaskListTests.cs
@@ -1,6 +1,5 @@
 ﻿using Application.TaskLists.Commands.CreateTaskList;
 using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Application.IntegrationTests.TaskLists.Commands;
@@ -9,7 +8,7 @@ public class CreateTaskListTests
 {
     [Fact(DisplayName = "DADO um comando vazio, QUANDO validar, DEVE retornar erro de campo obrigatório")]
     [Trait("Application", "Create - TaskList")]
-    public async Task InvalidCreateCommand_TryValidate_ShouldReturnErrors()
+    public void InvalidCreateCommand_TryValidate_ShouldReturnErrors()
     {
         // Arrange
         var command = new CreateTaskListCommand();
@@ -18,13 +17,13 @@ public class CreateTaskListTests
         var result = new CreateTaskListCommandValidator().Validate(command);
 
         // Assert
-        Assert.Equal(1, result.Errors.Count);
+        Assert.Equal(1, result.Errors?.Count);
         Assert.Equal("Title é obrigatório.", result.Errors.FirstOrDefault().ErrorMessage);
     }
 
-    [Fact(DisplayName = "DADO um comando com título inválido, QUANDO validar o comando, DEVE retornar erro de 256 caracteres")]
+    [Fact(DisplayName = "DADO um título inválido, QUANDO validar o comando, DEVE retornar erro de 256 caracteres")]
     [Trait("Application", "Create - TaskList")]
-    public async Task InvalidCreateCommand_TryValidateTitle_ShouldReturnErrors()
+    public void InvalidCreateCommand_TryValidateTitle_ShouldReturnErrors()
     {
         // Arrange
         var command = new CreateTaskListCommand
@@ -40,7 +39,7 @@ public class CreateTaskListTests
         var result = new CreateTaskListCommandValidator().Validate(command);
 
         // Assert
-        Assert.Equal(1, result.Errors.Count);
-        Assert.Equal("Title pode ter até 256 caracteres.", result.Errors.FirstOrDefault().ErrorMessage);
+        Assert.Equal(1, result.Errors?.Count);
+        Assert.Equal("Title pode ter até 256 caracteres.", result.Errors?.FirstOrDefault()?.ErrorMessage);
     }
 }
